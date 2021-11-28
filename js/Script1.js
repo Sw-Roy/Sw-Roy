ae78f2de7f1072d89ab626a4bf0988f1441787ec
Swarna Roy <royswarna90@gmail.com>
2021-11-28 23:03:04 +0530

    Final

--- js/Script1.js
+++ js/Script1.js
@@ -1,20 +1,4 @@
-/*
-      document.getElementById("myButton").onclick=function()
-      {
-        location.href = "show.html";
-      };
-    
-      var y =  document.getElementById("form");
-      y.style.display = "none";
-      function select() 
-      {
-          if (y.style.display === "none") 
-          {
-            y.style.display = "block";
-          }
-      }
 
-*/
 var obj={};
 var objArr=[];
 document.cookie = [];
@@ -30,9 +14,7 @@ $("#candidate-selection").change(function () {
 });
 
 $( "#profession-form" ).submit(function( event ) {
-  //console.log($('#email').val());
-  //console.log($('#work').val());
-  //console.log($("#candidate-selection").val());
+  
   if($('#email').value != "" || $('work').value != ""){
       console.log("passed");
       if(!duplicateSelectionCheck()){
